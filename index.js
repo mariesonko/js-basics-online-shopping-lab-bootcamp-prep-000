dfc761d62e800504aac7cbadae20b8a03f37b209
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-12 16:42:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   var emptyCart = []
-  if ( cardNumber !== cardNumber){
+  if ( cardNumber === undefined ){
     console.log ("Sorry, we don't have a credit card on file for you.")
   }else {
     console.log( `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`)
