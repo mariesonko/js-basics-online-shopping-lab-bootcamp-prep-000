5af508e3d740b2fc2894e95bcf711f7fdc7c4a9b
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-12 17:04:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item){
-  var newItem = name
-  var item = {[newItem] : name}
+  var itemName = name
+  var item = {[itemName] : name}
   var price = Math.floor(Math.random()*100)
   cart.push ({[item]:price})
   console.log (`${item} has been added to your cart.`)
